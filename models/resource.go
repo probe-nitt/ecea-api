62fc183e55fa247808656979953aa12d59636f5f
RevanthSPK <rspk2207@gmail.com>
2023-01-24 22:33:03 +0530

    feat(models): added study material and subject models

---
+++ models/resource.go
@@ -0,0 +1,33 @@
+package models
+
+type StudyMaterialRequest struct {
+	Name            string `json:"name" form:"name"`
+	Subject         string `json:"subject" form:"subject"`
+	SubjectCategory string `json:"subject_category" form:"category"`
+	SubjectCode     string `json:"subject_code" form:"code"`
+}
+type StudyMaterials struct {
+	Name            string `json:"name"`
+	Subject         string `json:"subject"`
+	SubjectCategory string `json:"subject_category"`
+	SubjectCode     string `hson:"subject_code"`
+	DocumentURL     string `json:"document_url"`
+}
+
+type CategoryMaterials struct {
+	Category string           `json:"category"`
+	Material []StudyMaterials `json:"subjects"`
+}
+
+type SubjectCategory string
+
+const (
+	DigitalElectronics           SubjectCategory = "DIGITAL ELECTRONICS"
+	AnalogElectronics            SubjectCategory = "ANALOG ELECTRONICS"
+	Telecommunication            SubjectCategory = "TELECOMMUNICATION"
+	CommunicationChanneling      SubjectCategory = "COMMUNICATION CHANNELING"
+	SystemDesignAndArchitechture SubjectCategory = "SYSTEM DESIGN AND ARCHITECTURE"
+	BasicEngineering             SubjectCategory = "BASIC ENGINEERING"
+	Mathematics                  SubjectCategory = "MATHEMATICS"
+	Others                       SubjectCategory = "OTHERS"
+)
